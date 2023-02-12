fe6a994fb656fae4f3a5dd31ebb132c4aaa6b7b6
cristianleit <124408277+CristianLeit@users.noreply.github.com>
2023-02-12 00:19:30 -0300

    ignore

---
+++ node/moduloA.js
@@ -0,0 +1,5 @@
+console.log(this);
+
+this.ola = 'Fala Pessoal'
+exports.bemVindo = 'Bem Vindo ao node!'
+module.exports.ateLogo = 'Até próximo exemplo'
---
+++ node/moduloB.js
@@ -0,0 +1,8 @@
+let a = 1
+
+module.exports = {
+    bomDia : 'Bom dia',
+    boaNoite(){
+        return 'Boa noite'
+    }
+}
