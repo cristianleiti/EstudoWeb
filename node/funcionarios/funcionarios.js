ff3198624b0e190aa12021170a26920d932eb20c
cristianleit <124408277+CristianLeit@users.noreply.github.com>
2023-02-13 23:11:03 -0300

    9

--- node/funcionarios/funcionarios.js
+++ node/funcionarios/funcionarios.js
@@ -1,7 +1,15 @@
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const axios = require('axios')
 
-axios.get(url).then(response =>{
+const chineses = (p) => p.pais === 'China'
+const mulheres = (g) => g.genero === 'F'
+const menor = (s1, s2) => s1.salario < s2.salario ? s1 : s2
+
+axios.get(url).then(response => {
     const funcionarios = response.data
-    console.log(funcionarios);
+
+    const chineFem = funcionarios.filter(chineses)
+        .filter(mulheres)
+        .reduce(menor)
+    console.log(chineFem);
 })
